717cf216d4fb38f3fbd58f4a444051c1a3fc1de3
benhigginbottom <ben.higginbottom@gmail.com>
2016-03-30 22:47:31 +0100

    Forced data retrieval policy to be free by default

--- glacier.go
+++ glacier.go
@@ -1,33 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/glacier"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/glacier"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
 
-    //Get the names of all the directories in our current root
-    // these will be the vault names
-    vnames, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
-    }
-    //vaultName := "gotest1"
+	//Get the names of all the directories in our current root
+	// these will be the vault names
+	vnames, valerr := ioutil.ReadDir("./")
+	if valerr != nil {
+		log.Fatal(valerr)
+	}
+	//vaultName := "gotest1"
 
-    svc := glacier.New(session.New(&aws.Config{Region: aws.String("eu-west-1")}))
-    for _, vaultName := range vnames {
-        _, err := svc.CreateVault(&glacier.CreateVaultInput{
-        VaultName: aws.String(vaultName.Name()),
-        })
-    if err != nil {
-    log.Println(err)
-    return
-}
-fmt.Println("Created vault", vaultName)
-}
+	//Setup the policy for data retrieval - HT Andrew
+	params := &glacier.SetDataRetrievalPolicyInput{
+		Policy: &glacier.DataRetrievalPolicy{
+			Rules: []*glacier.DataRetrievalRule{
+				&glacier.DataRetrievalRule{
+					Strategy: aws.String("FreeTier"),
+				},
+			},
+		},
+	}
+
+	svc := glacier.New(session.New(&aws.Config{Region: aws.String("eu-west-1")}))
+	pol, polerr := svc.SetDataRetrievalPolicy(params)
+	if polerr != nil {
+		fmt.Println(polerr)
+		return
+	}
+	fmt.Println(pol)
+	//And now setup the vaults
+	for _, vaultName := range vnames {
+		_, err := svc.CreateVault(&glacier.CreateVaultInput{
+			VaultName: aws.String(vaultName.Name()),
+		})
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println("Created vault", vaultName)
+	}
 }
